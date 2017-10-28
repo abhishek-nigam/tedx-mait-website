3ee058eaa57972489f8d71934c2cba9cb7e9e077
TEDxMAIT <tedxmait.delhi@gmail.com>
2017-10-28 21:03:53 +0530

    Added preview

---
+++ preview/assets/js/custom_scripts.js
@@ -0,0 +1,47 @@
+/**
+ * =======================
+ * Code for countdown timer
+ * ======================== 
+ */
+
+function updateTimer(deadline) {
+    var time = deadline - new Date();
+    return {
+        'days': Math.floor( time/(1000*60*60*24) ),
+        'hours': Math.floor( (time/(1000*60*60)) %24 ),
+        'minutes': Math.floor( (time/1000/60) %60),
+        'seconds': Math.floor( (time/1000) % 60),
+        'total': time
+    };
+}
+
+function startTimer(id, deadline) {
+    var timerInterval = setInterval(function(){
+        var clock = document.getElementById("countdown-clock");
+        var counts = clock.getElementsByClassName("count")
+
+        var timer = updateTimer(deadline);
+
+        counts[0].innerHTML = timer.days;
+        counts[1].innerHTML = timer.hours;
+        counts[2].innerHTML = timer.minutes;
+        counts[3].innerHTML = timer.seconds;
+        
+
+        // check for end of time
+        if(timer.total < 1) {
+            clearInterval(timerInterval);
+            
+            counts[0].innerHTML = timer.days;
+            counts[1].innerHTML = timer.hours;
+            counts[2].innerHTML = timer.minutes;
+            counts[3].innerHTML = timer.seconds;
+
+        }
+    }, 1000);
+}
+
+window.onload = function(){
+    var deadline = new Date("March 17, 2018 10:00:00");
+    startTimer("clock",deadline);
+}
---
+++ preview/assets/js/navigation_smooth_scrolling.js
@@ -0,0 +1,20 @@
+$(function() {
+  var topoffset = 71; //variable for menu height
+
+  //Use smooth scrolling when clicking on navigation
+  $('.navbar-nav a').click(function() {
+    if (location.pathname.replace(/^\//,'') === 
+      this.pathname.replace(/^\//,'') && 
+      location.hostname === this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top-topoffset
+        }, 500);
+        return false;
+      } //target.length
+    } //click function
+  }); //smooth scrolling
+
+});
